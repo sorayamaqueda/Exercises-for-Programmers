6dc7a1ced504ace0d9f61a80a648c94541e7de17
soraya <A01411195@itesm.mx>
2019-12-05 14:18:03 -0600

    Exercise 5

---
+++ cmake-build-debug/CMakeFiles/SimpleMath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleMath.dir/main.cpp.o"
+  "SimpleMath"
+  "SimpleMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Write a program that prompts for tow numbers. Print the sum, difference and quotient of those numbers as shown in the exapmle.
+ * Challenges:
+ * Revise that the inputs are numeric value
+ * Do not proceed if the enterd value is negative.
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
